a1f51c13f7120e5af60ef312a9bbedb2110adf28
Brian Flannery <bflannery66@gmail.com>
2017-01-15 12:46:34 -0600

    updated description

--- app/scripts/Collections/projectsCollection.js
+++ app/scripts/Collections/projectsCollection.js
@@ -7,11 +7,8 @@ let projects = [
     image: '../../assets/images/tulip/tulip-1.png',
     imgHover: '../../assets/images/tulip/tulip-2.png',
     projectImages: ['../../assets/images/tulip/tulip-1.png', '../../assets/images/tulip/tulip-2.png', '../../assets/images/tulip/tulip-3.png', '../../assets/images/tulip/tulip-4.png', '../../assets/images/tulip/tulip-5.png'],
-    pitch: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    description: '',
-    prompt: '',
-    best: '',
-    hardest: ''
+    pitch: 'Tulip & Ivy is an Austin based floral arrangements company that specializes in events, corporate, and personal florals',
+    features: ['React LightBox Gallery, Linked Instagram Account, Connected Contact Form']
       },
   {
     id: 'two',
@@ -21,11 +18,8 @@ let projects = [
     image: '../../assets/images/gym/gym-1.png',
     imgHover: '../../assets/images/gym/gym-2.png',
     projectImages: ['../../assets/images/gym/gym-1.png', '../../assets/images/gym/gym-2.png', '../../assets/images/gym/gym-3.png', '../../assets/images/gym/gym-4.png'],
-    pitch: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    description: '',
-    prompt: '',
-    best: '',
-    hardest: ''
+    pitch: 'Gym Tracker was designed to help personal trainers organize, plan, and track their athletes progress through workouts and cycles over an extended period of time',
+    features: ['Under Armour API, Secure Register/Login, Relational Data, Athlete Profile']
   },
   {
     id: 'three',
@@ -35,11 +29,8 @@ let projects = [
     image: '../../assets/images/band/band-1.png',
     imgHover: '../../assets/images/band/band-3.png',
     projectImages: ['../../assets/images/band/band-1.png', '../../assets/images/band/band-3.png', '../../assets/images/band/band-4.png'],
-    pitch: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    description: '',
-    prompt: '',
-    best: '',
-    hardest: ''
+    pitch: 'Using Spotifys API, Bring The Band is an app that allows locals to vote for the bands they want to see in their cities.',
+    features: ['Spotify API, Secure Register/Login, Geolocation']
   },
   {
     id: 'four',
@@ -49,11 +40,8 @@ let projects = [
     image: '../../assets/images/knowthings/jeo-1.png',
     imgHover: '../../assets/images/knowthings/jeo-2.png',
     projectImages: ['../../assets/images/knowthings/jeo-1.png', '../../assets/images/knowthings/jeo-2.png', '../../assets/images/knowthings/jeo-3.png', '../../assets/images/knowthings/jeo-4.png'],
-    pitch: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    description: '',
-    prompt: '',
-    best: '',
-    hardest: ''
+    pitch: 'Jeopardy game that renders random categories and questions using Jeopardys API containing clues and questions.',
+    features: ['Jeopardy API, React Components, Point Tracking']
     }
 ];
 
